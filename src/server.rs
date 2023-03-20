8749c42eabb8bda581d5cf4a9b796af6b45bda9d
Shawn M Moore <code@sartak.org>
2023-03-20 15:05:36 -0400

    Validate event type

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,5 @@
 use anyhow::{Context, Result};
-use axum::{extract::Query, response::IntoResponse, routing::get, Router};
+use axum::{extract::Query, http::StatusCode, response::IntoResponse, routing::get, Router};
 use log::info;
 use serde::Deserialize;
 
@@ -23,6 +23,21 @@ struct GameParams {
 }
 
 async fn game_get(Query(params): Query<GameParams>) -> impl IntoResponse {
+    let event = match params.event.as_str() {
+        "start" => (),
+        "end" => (),
+        _ => {
+            info!(
+                "GET /game {params:?} -> 400 (invalid event: {})",
+                params.event
+            );
+            return (
+                StatusCode::BAD_REQUEST,
+                format!("invalid event: {}", params.event),
+            )
+                .into_response();
+        }
+    };
     info!("GET /game {params:?} -> 200");
-    format!("Hello, /game event={} file={}", params.event, params.file)
+    format!("Hello, /game event={} file={}", params.event, params.file).into_response()
 }
