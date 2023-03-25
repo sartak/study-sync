1bb40f176ce517afc9f3b7eab9e22a603aa62c34
Shawn M Moore <code@sartak.org>
2023-03-25 15:48:26 -0400

    Add corresponding structs

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,30 @@
 use std::path::PathBuf;
 
 #[derive(Debug)]
+pub enum Language {
+    English,
+    Japanese,
+    Cantonese,
+    Other(String),
+}
+
+#[derive(Debug)]
 pub struct Game {
+    pub id: i64,
     pub path: PathBuf,
+    pub directory: String,
+    pub language: Language,
+    pub label: String,
+}
+
+#[derive(Debug)]
+pub struct Play {
+    pub id: i64,
+    pub game: Game,
+    pub start_time: u64,
+    pub end_time: Option<u64>,
+    pub intake_id: Option<u64>,
+    pub submitted_start: Option<u64>,
+    pub submitted_end: Option<u64>,
+    pub skipped: bool,
 }
