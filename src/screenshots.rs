a323a16da0809440a1884189c968e874f43c4813
Shawn M Moore <code@sartak.org>
2023-03-26 17:10:10 -0400

    First cut of uploading screenshots

--- src/screenshots.rs
+++ src/screenshots.rs
@@ -1,7 +1,11 @@
 use anyhow::Result;
-use log::info;
-use std::path::PathBuf;
+use log::{error, info};
+use reqwest::{multipart, Body};
+use std::path::{Path, PathBuf};
+use std::time::Duration;
+use tokio::fs::{remove_file, File};
 use tokio::sync::mpsc;
+use tokio_util::codec::{BytesCodec, FramedRead};
 
 #[derive(Debug)]
 pub enum Event {
@@ -37,9 +41,55 @@ impl Screenshots {
         while let Some(event) = self.rx.recv().await {
             info!("Handling event {event:?}");
             match event {
-                Event::UploadScreenshot(path, directory) => {}
+                Event::UploadScreenshot(path, directory) => {
+                    self.upload_path_to_directory(&path, directory).await;
+                    if let Err(e) = remove_file(&path).await {
+                        error!("Could not remove uploaded screenshot file {path:?}: {e:?}");
+                    }
+                }
             }
         }
         Ok(())
+    }
+
+    fn agent(&self) -> reqwest::Client {
+        let builder = reqwest::ClientBuilder::new().timeout(Duration::from_secs(30));
+        builder.build().unwrap()
+    }
+
+    async fn upload_path_to_directory(&self, path: &Path, directory: String) -> () {
+        let url = format!("{}/{directory}", self.screenshot_url);
+        loop {
+            match File::open(path).await {
+                Ok(file) => {
+                    let stream = FramedRead::new(file, BytesCodec::new());
+                    let file_body = Body::wrap_stream(stream);
+                    let part = multipart::Part::stream(file_body);
+                    let form = multipart::Form::new().part("screenshot", part);
+                    match self.agent().post(&url).multipart(form).send().await {
+                        Ok(res) => {
+                            match res.text().await {
+                                Ok(message) => {
+                                    info!("Successfully uploaded {path:?} to {directory:?}: {message}");
+                                    break;
+                                }
+                                Err(e) => {
+                                    error!("Failed to parse upload response for {path:?} into text: {e:?}");
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            error!("Failed to upload {path:?} for {directory:?}: {e:?}");
+                        }
+                    }
+                }
+                Err(e) => {
+                    error!("Could not open {path:?} for reading: {e:?}");
+                }
+            }
+
+            info!("Sleeping for 5s before trying again");
+            tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+        }
     }
 }
